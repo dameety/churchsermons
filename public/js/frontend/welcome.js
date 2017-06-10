b5e7e191fb5f397113cb4b9271c00b96ad5262f8
Dameety <djdamilola01@gmail.com>
2017-06-10 03:29:42 +0100

    public folder corrections

---
+++ public/js/frontend/welcome.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	center: true,
+  	loop: true,
+  	autoplay: true,
+  	autoplayHoverPause: false,
+  	autoplaySpeed: 2000,
+  	autoplayTimeout: 2000,
+  });
+});
+
