1f94cc9788e249fa432fa299477b1492246a39f5
Dameety <djdamilola01@gmail.com>
2017-06-21 00:00:06 +0100

    change auth redirect route

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/sermons';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
