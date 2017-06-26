dc6b6a7658cfb742a3628365dff63204a786753b
Dameety <djdamilola01@gmail.com>
2017-06-27 00:49:19 +0100

    .

--- config/database.php
+++ config/database.php
@@ -65,8 +65,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'strict' => false
         ],
 
         'pgsql' => [
