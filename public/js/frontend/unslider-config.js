c2e2b1b7222ef190c9f67a1c83364999c61ddb0b
Dameety <djdamilola01@gmail.com>
2017-07-06 23:33:05 +0100

    add unslider.js

---
+++ public/js/frontend/unslider-config.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('.banner').unslider({
+        animation: 'vertical',
+        autoplay: true,
+        infinite: true,
+        nav: false,
+        arrows: false
+    })
+
+    $('.testimonials').unslider({
+        autoplay: true,
+        infinite: true,
+        arrows: false,
+        nav: false
+    })
+
+
+
+});
