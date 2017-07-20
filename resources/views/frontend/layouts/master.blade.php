be10685ec5f9220cf9234954be95423636aab772
dameety <djdamilola01@gmail.com>
2017-07-20 22:56:17 +0100

    clean up

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -8,9 +8,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name') }} - @yield('title')</title>
 
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="{{ URL::asset('/css/simple-line-icons.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('/css/uikit.min.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('/css/frontend/responsive.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('/css/frontend/style.css') }}">
